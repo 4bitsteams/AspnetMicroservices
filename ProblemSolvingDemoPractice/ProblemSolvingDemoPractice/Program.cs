1d3545bc4ad9d6c235fc4de507b013af17310110
Md. Rubel 1764 <md.rubel@bracits.com>
2024-02-19 23:42:34 +0600

    MultiDimention Array Example

--- ProblemSolvingDemoPractice/ProblemSolvingDemoPractice/Program.cs
+++ ProblemSolvingDemoPractice/ProblemSolvingDemoPractice/Program.cs
@@ -8,9 +8,26 @@ namespace ProblemSolvingDemoPractice
         {
             ArraysDemo();
             Test1BaseArray();
+            MultiDimArrays();
             Console.ReadKey();
         }
 
+        private static void MultiDimArrays()
+        {
+            int[,] arr = new int[,] { { 1, 2, 3 }, { 3, 2, 1 } };
+
+            int[,] arr2 = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.WriteLine(arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+        }
         private static void ArraysDemo()
         {
             int[] a1;
@@ -32,16 +49,16 @@ namespace ProblemSolvingDemoPractice
             }
             Console.WriteLine();
 
-            Array array=new int[5];
-            Array array2=Array.CreateInstance(typeof(int), 5);
-            array2.SetValue(10,0);
+            Array array = new int[5];
+            Array array2 = Array.CreateInstance(typeof(int), 5);
+            array2.SetValue(10, 0);
 
-           Console.ReadKey();
+            Console.ReadKey();
         }
 
         private static void Test1BaseArray()
         {
-            Array array=Array.CreateInstance(typeof(int), new[] { 4 }, new[] {1});
+            Array array = Array.CreateInstance(typeof(int), new[] { 4 }, new[] { 1 });
             array.SetValue(2019, 1);
             array.SetValue(2020, 2);
             array.SetValue(2021, 3);
@@ -51,7 +68,7 @@ namespace ProblemSolvingDemoPractice
             Console.WriteLine($"Ending Index {array.GetUpperBound(0)}");
 
             //for (int i = array.GetLowerBound(0); i < =array.GetUpperBound(0); i++)
-            for (int i = 0;i < array.Length;i++)    
+            for (int i = 0; i < array.Length; i++)
             {
                 Console.WriteLine($"{array.GetValue(i)} at index {i}");
             }
