a89701c17b5af2df5a9c9d3ae25d1c03cb470b44
Md. Rubel 1764 <md.rubel@bracits.com>
2024-02-21 01:25:31 +0600

    Implement Bubble Sort and Add Test Project

---
+++ ProblemSolvingDemoPractice/ProblemSolvingDemoPractice/Sorting/Sorting.cs
@@ -0,0 +1,27 @@
+﻿namespace ProblemSolvingDemoPractice.Sorting
+{
+    public class Sorting
+    {
+        public static void BubbleSort(int[] array)
+        {
+            for (int partIndex = array.Length - 1; partIndex > 0; partIndex++)
+            {
+                for (int i = 0; i < partIndex; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        Swap(array, i, i + 1);
+                    }
+                }
+            }
+        }
+        private static void Swap(int[] array, int i, int j)
+        {
+            if (i == j)
+                return;
+            int tempValue = array[i];
+            array[i] = array[j];
+            array[j] = tempValue;
+        }
+    }
+}
